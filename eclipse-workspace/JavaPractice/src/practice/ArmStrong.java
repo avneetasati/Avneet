1421576cb2fb3b19840207b7b0971ebb06b37868
avneetasati <avneetasati@gmail.com>
2018-01-26 20:47:37 +0530

    Commit done

---
+++ eclipse-workspace/JavaPractice/src/practice/ArmStrong.java
@@ -0,0 +1,28 @@
+package practice;
+
+public class ArmStrong {
+
+	
+	public static void main(String [ ]args) {
+		
+		int r,c=0;
+		int n=153;
+		int temp = n;
+				while(n>0)
+				{
+					r=n%10;
+					n=n/10;
+					c=c+(r*r*r);
+				}
+				if(temp==c) {
+				
+					System.out.println("No is armstrong no.");
+				}
+				
+				else {
+					System.out.println("Not a armstrong no.");
+					
+				}
+		
+	}
+}
